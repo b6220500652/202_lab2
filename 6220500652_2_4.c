c67f36e87936531e05335809d4d22e5951ac71ec
b6220500652 <narisara.kaa@ku.th>
2021-02-11 17:36:15 +0700

    emoji

--- 6220500652_2_4.c
+++ 6220500652_2_4.c
@@ -1,19 +1,31 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-	char x;
-	int a;
-	scanf("%c",&x);
-	scanf("%d",&a);
-	if(x=='A'||x=='B'||x=='C'||x=='D'||x=='E'||x=='F'||x=='G'||x=='H'||x=='I')
-	{
-		if(a%2!=0)
-		{
-			printf("(^_^)");
-		}
-		else
-		{
-			printf("{@_@}");
-		}
+    int x,y;
+    char a;
+    char emoji[2][3][6] = {{"(^_^)","(*o*)","(T_T)"},{"{@_@}","{*v*}","{x_x}"}};
+    char z[6];
+    scanf("%c",&a);
+    scanf("%d",&x);
+    y=a+1-x;
+    if(a>='A'&&a<='I')
+    {
+    	strcpy(z,emoji[y%2][0]);
+	}  
+    else if(a>='J'&&a<='R')
+    {
+    	strcpy(z,emoji[y%2][1]);
 	}
+    else
+        strcpy(z,emoji[y%2][2]);
+        
+    if(x%10==5)
+    {
+    	printf("%c%s/\n",92,z);
+	}
+    else
+        puts(z);
+
+    return 0;
 }
