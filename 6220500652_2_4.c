83a764f3fc57e359c1e73d35462d267ab4f628f9
b6220500652 <narisara.kaa@ku.th>
2021-02-11 16:34:32 +0700

    finish

---
+++ 6220500652_2_4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	char x;
+	int a;
+	scanf("%c",&x);
+	scanf("%d",&a);
+	if(x=='A'||x=='B'||x=='C'||x=='D'||x=='E'||x=='F'||x=='G'||x=='H'||x=='I')
+	{
+		if(a%2!=0)
+		{
+			printf("(^_^)");
+		}
+		else
+		{
+			printf("{@_@}");
+		}
+	}
+}
